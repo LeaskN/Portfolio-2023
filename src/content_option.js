6101da9986a5b21095282deddf160a707f91577a
Nicholas Leask <leask.nicholas@gmail.com>
2023-07-06 16:05:54 -0400

    Info: added information regarding work history, services, skills, etc..

--- src/content_option.js
+++ src/content_option.js
@@ -21,40 +21,40 @@ const dataabout = {
     aboutme2: `I'm always in the market for exciting opportunities, freelance, traditional employment, or something else!`,
 };
 const worktimeline = [{
-        jobtitle: "Software Engineer II",
-        where: "Asurion Insurance Services",
-        date: "2022 - Present",
-    },
-    {
-        jobtitle: "Software Engineer | Leadership Team",
-        where: "Nebula Academy",
-        date: "2018 - 2022",
-    },
-    {
-        jobtitle: "Software Engineer",
-        where: "Unlock-The-Box",
-        date: "2018 - 2022",
-    },
-    {
-        jobtitle: "Full Stack Web Development Student",
-        where: "Galvanize NYC",
-        date: "2017 - 2018",
-    },
-    {
-        jobtitle: "Datastructures & Algorithms",
-        where: "App Academy Bootcamp Prep NYC",
-        date: "2017",
-    },
-    {
-        jobtitle: "Python Learning",
-        where: "Self Taught Python Learning Course",
-        date: "2017",
-    },
-    {
-        jobtitle: "Tech Recruitment Consultant",
-        where: "Nigel Frank Recruitment, NYC",
-        date: "2017",
-    }
+    jobtitle: "Software Engineer II",
+    where: "Asurion Insurance Services",
+    date: "2022 - Present",
+},
+{
+    jobtitle: "Software Engineer | Leadership Team",
+    where: "Nebula Academy",
+    date: "2018 - 2022",
+},
+{
+    jobtitle: "Software Engineer",
+    where: "Unlock-The-Box",
+    date: "2018 - 2022",
+},
+{
+    jobtitle: "Full Stack Web Development Student",
+    where: "Galvanize NYC",
+    date: "2017 - 2018",
+},
+{
+    jobtitle: "Datastructures & Algorithms",
+    where: "App Academy Bootcamp Prep NYC",
+    date: "2017",
+},
+{
+    jobtitle: "Python Learning",
+    where: "Self Taught Python Learning Course",
+    date: "2017",
+},
+{
+    jobtitle: "Tech Recruitment Consultant",
+    where: "Nigel Frank Recruitment, NYC",
+    date: "2017",
+}
 ];
 
 const skills = [
@@ -164,6 +164,13 @@ const skills = [
         id: 15
     },
     {
+        name: "Java",
+        value: 50,
+        importance: 6,
+        experience: `${new Date(new Date() - new Date('2022/06/01')).getFullYear() - 1970}`,
+        id: 15
+    },
+    {
         name: "Postman",
         value: 100,
         importance: 6,
@@ -187,80 +194,103 @@ const skills = [
 ];
 
 const services = [{
-        title: "UI & UX Design",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
-    },
-    {
-        title: "Mobile Apps",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
-    },
-    {
-        title: "Wordpress Design",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
-    },
+    title: "Front-End Development",
+    descriptions: [
+        `● Created web experiences including gamified learning experiences, landing pages, sign-up pages, admission forms, loading screens, informational pages, blogs, and more.`,
+        `● Expanded functionality using JavaScript, React, and SVGs, by allowing users to see other teammates actions,
+        status, and completion of questions, minimizing negative user feedback on completed questions by 80%.`,
+        `● Improved user interface and user experience with JavaScript, React, and CSS, showing all teams live positions and winning animations for 1st to 3rd places, reducing negative scoreboard feedback to 0%.`,
+        `● Crafted web forms to enable participant signup for learning programs utilizing JavaScript, React, and AWS, supporting expansion into new programs and increasing existing program participants by over 6X.`,
+        `● Increased average web-viewership time by 300% using JavaScript, React, and AWS (EC2, S3, RDS, CloudFront, Route 53), growing web traffic, program participants, and revenue with 2X increase YoY 2 years in a row.`,
+        `● Developed data redundancy to ensure raw user information was being captured utilizing JavaScript, React, and AWS (RDS, EC2), ensuring user information was not lost at any point during the sales cycle.`,
+        `● Created landing and sign up pages with an increased Call To Action (CTA).`
+    ]
+},
+{
+    title: "Back-End Development",
+    descriptions: [
+        `● Migrated several existing company APIs away from a monolithic codebase from Java to Node.js utilizing JavaScript, TypeScript, and Express.js, reducing costs of on-site servers by an estimated 90%.`,
+        `● Created and updated field technician user events providing more accurate tracking to our partners and clients. This enabled field technicians to have greater potential to up-sell clients, resulting in 30% more sales.`,
+        `● Migrated features of an acquired company with our own APIs enabling new billion-dollar clients the ability to interact with the clients APIs, ultimately integrating a new multi-million-dollar revenue stream.`,
+        `● Developed and implemented unit tests that identified hundreds of bugs pre-deployment to QA or production increasing development and QA testing speed by approximately 25%.`,
+        `● Updated a startups backend infrastructure to increase speed, decrease development time, and decrease downtime to near-zero.`,
+    ]
+},
+// {
+//     title: "WordPress",
+//     descriptions: [
+//         `● Developed a clients website allowing them to sell products, improve web presence through SEO, and gather visitor information through email signup forms.`
+//     ]
+// },
+// {
+//     title: "SquareSpace",
+//     descriptions: [
+//         `● Developed a static website for a client to provide potential customers with additional information.`
+//     ]
+// },
 ];
 
 const dataportfolio = [{
-        img: "https://picsum.photos/400/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/800/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/600/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/300/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/700/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
+    img: "https://picsum.photos/400/?grayscale",
+    description: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/800/?grayscale",
+    description: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/?grayscale",
+    description: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/600/?grayscale",
+    description: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/300/?grayscale",
+    description: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/700/?grayscale",
+    description: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
 
-    {
-        img: "https://picsum.photos/400/600/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/300/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/550/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/700/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
+{
+    img: "https://picsum.photos/400/600/?grayscale",
+    description: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/300/?grayscale",
+    description: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/?grayscale",
+    description: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/550/?grayscale",
+    description: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/?grayscale",
+    description: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/700/?grayscale",
+    description: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
 ];
 
 const contactConfig = {
